c6638ac575d3876d946212d488908e7ae8053e43
GCLiang <liangguangcan20031225@outlook.com>
2024-10-23 18:56:08 +0800

    lab7fourth

--- lab07/src/RedBlackTree.java
+++ lab07/src/RedBlackTree.java
@@ -74,9 +74,13 @@ public class RedBlackTree<T extends Comparable<T>> {
         temp.left = temp.left.right;//待处理的左指向原左的右，(坏了，这里直接把node给改了)
         node = node.left;//改变，用待处理的左去覆盖
         node.right = temp;//覆盖后的右指向前面的处理过的记录节点
-        //上面完成了旋转操作，还有颜色未改
-        node.isBlack = false;
-        temp.isBlack = true;
+        //上面完成了旋转操作，还有颜色未改(交换！交换！颜色)
+        //node.isBlack = true;
+        //temp.isBlack = false;
+        boolean temcolorOFtemp = temp.isBlack;
+        boolean temcolorOFnode = node.isBlack;
+        node.isBlack = temcolorOFtemp;
+        temp.isBlack = temcolorOFnode;
         return node;
     }
 
@@ -94,9 +98,13 @@ public class RedBlackTree<T extends Comparable<T>> {
         node = node.right;//改变，用待处理的you去覆盖
         node.left = temp;//覆盖后的右指向前面的处理过的记录节点
         //上面完成了旋转操作，还有颜色未改
-        node.isBlack = false;
-        temp.isBlack = true;
-        return node;
+        //node.isBlack = true;
+        //temp.isBlack = false;
+        boolean temcolorOFtemp = temp.isBlack;
+        boolean temcolorOFnode = node.isBlack;
+        node.isBlack = temcolorOFtemp;
+        temp.isBlack = temcolorOFnode;
+        return node;//有问题啊，这里的旋转只能是特定的形状作特定的旋转(好像又没有错，淦)
     }
 
     /**
@@ -128,28 +136,50 @@ public class RedBlackTree<T extends Comparable<T>> {
      */
     private RBTreeNode<T> insert(RBTreeNode<T> node, T item) {
         // TODO: Insert (return) new red leaf node.
+        RBTreeNode<T> hadInseretedNode;//最后return这个出去
         if (node == null) {
-            return new RBTreeNode(false, item);
+            hadInseretedNode =  new RBTreeNode(false, item);
         }
 
         // TODO: Handle normal binary search tree insertion.
-        else {
+        /*else {
             if (item.compareTo(node.item) > 0) {
                 return new RBTreeNode<>(node.isBlack, node.item, node.left, insert(node.right, item));
             }
             else {
                 return new RBTreeNode<>(node.isBlack, node.item, insert(node.left, item), node.right);
             }
+        }*/
+        else {
+
+            hadInseretedNode = new RBTreeNode(node.isBlack, node.item);
+            if (item.compareTo(node.item) > 0) {
+                hadInseretedNode.left = node.left;
+                hadInseretedNode.right = insert(node.right, item);
+            } else {
+                hadInseretedNode.left = insert(node.left, item);
+                hadInseretedNode.right = node.right;
+            }
+
+
+            // TODO: Rotate left operation
+            if (item.compareTo(hadInseretedNode.item) > 0 && !isRed(hadInseretedNode.left)) {
+                hadInseretedNode = rotateLeft(hadInseretedNode);
+            }
+
+            // TODO: Rotate right operation
+            if (isRed(hadInseretedNode.left.left) && isRed(hadInseretedNode.left)) {
+                hadInseretedNode = rotateRight(hadInseretedNode);
+
+            }
+
+            // TODO: Color flip
+            if (isRed(hadInseretedNode.left) && isRed(hadInseretedNode.right)) {
+               flipColors(hadInseretedNode);
+            }
+
         }
-
-        // TODO: Rotate left operation
-
-        // TODO: Rotate right operation
-
-        // TODO: Color flip
-
-
-         //fix this return statement
+         return hadInseretedNode;//fix this return statement
     }
 
 }
